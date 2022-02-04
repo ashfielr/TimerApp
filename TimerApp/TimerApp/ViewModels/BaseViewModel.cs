5d16e498b3da956455af77da45b7480341b8d7fc
Robert Ashfield <ashfielr@uni.coventry.ac.uk>
2022-02-04 14:26:39 +0000

    Add project files.

---
+++ TimerApp/TimerApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace TimerApp.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var args = new PropertyChangedEventArgs(propertyName);
+            PropertyChanged?.Invoke(this, args);
+        }
+        #endregion
+
+    }
+}
---
+++ TimerApp/TimerApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TimerApp.ViewModels
+{
+    internal class MainPageViewModel : BaseViewModel
+    {
+        private List<int> _numberedList = Enumerable.Range(0, 61).ToList();
+
+        public List<int> NumberedList { get => _numberedList; }
+        public int SelectedSec { get; set; }
+        public int SelectedMins { get; set; }
+        public int SelectedHours { get; set; }
+
+    }
+}
