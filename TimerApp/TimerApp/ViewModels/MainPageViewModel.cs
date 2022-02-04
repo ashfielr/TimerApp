d7fa3ac8d9130d0e4d13c6a9f97ada94792d2f51
Robert Ashfield <ashfielr@uni.coventry.ac.uk>
2022-02-04 14:46:32 +0000

    Added bindings

--- TimerApp/TimerApp/ViewModels/MainPageViewModel.cs
+++ TimerApp/TimerApp/ViewModels/MainPageViewModel.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace TimerApp.ViewModels
 {
     internal class MainPageViewModel : BaseViewModel
     {
-        private List<int> _numberedList = Enumerable.Range(0, 61).ToList();
+        public int TimerSec { get; set; }
+        public int TimerMins { get; set; }
+        public int TimerHrs { get; set; }
 
-        public List<int> NumberedList { get => _numberedList; }
-        public int SelectedSec { get; set; }
-        public int SelectedMins { get; set; }
-        public int SelectedHours { get; set; }
+        public int Sec { get; set; }
+        public int Mins { get; set; }
+        public int Hrs { get; set; }
+
+        public MainPageViewModel()
+        {
+
+        }
+
+        public ICommand SetTimerCommand { get; set; }
+        public ICommand ClearTimerCommand { get; set; }
+        public ICommand StartTimerCommand { get; set; }
+        public ICommand StopTimerCommand { get; set; }
 
     }
 }
